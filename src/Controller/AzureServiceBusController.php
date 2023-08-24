7d13d80de4acaf15c3d715c85c90c4a1e6e83c07
yjimenez <yjimenez@sonnysdirect.com>
2023-08-24 17:55:06 -0400

    Azure Service Bus and Azure Blob Storage implementation

--- src/Controller/AzureServiceBusController.php
+++ src/Controller/AzureServiceBusController.php
@@ -12,6 +12,7 @@ use Monolog\Logger;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/azure-service-bus")
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -9,6 +9,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/")
+ */
 class IndexController extends AbstractController
 {
     /**
